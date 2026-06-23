443a94bf73dbc3a6ff58764f8b08f3e190539ddf
alexandre-silvaa <alexandrebsilva9@gmail.com>
2026-06-23 20:49:03 -0300

    🔧 chore: ajustar importações e formatação no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { HeroSection } from './components/sections/HeroSection'
-import { inviteData } from './data/weeding-data'
-import { tw } from './styles/tw'
+import { EventSection } from "./components/sections/EventSection";
+import { HeroSection } from "./components/sections/HeroSection";
+import { StorySection } from "./components/sections/StorySection";
+import { inviteData } from "./data/weeding-data";
+import { tw } from "./styles/tw";
 
 function App() {
   return (
     <div className={tw.app}>
       <HeroSection data={inviteData} />
-      {/* <StorySection data={inviteData} />
-      <GamesSection data={inviteData} />
+      <StorySection data={inviteData} />
+      {/* <GamesSection data={inviteData} /> */}
       <EventSection data={inviteData} />
-      <GiftsSection data={inviteData} />
-      <RsvpSection data={inviteData} />
-      <GallerySection />
-      <FooterSection data={inviteData} /> */}
+      {/* <GiftsSection data={inviteData} /> */}
+      {/* <RsvpSection data={inviteData} /> */}
+      {/* <GallerySection /> */}
+      {/* <FooterSection data={inviteData} /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
